523b6543047f4c2892aaa505f58cd498df7f6db6
deyin <deyinx.chen@gmail.com>
2015-07-23 11:48:17 +0800

    Add un-commited files.

---
+++ src/com/cloudaward/lyl/consts/AppPrefsConsts.java
@@ -0,0 +1,13 @@
+package com.cloudaward.lyl.consts;
+
+public class AppPrefsConsts {
+
+  public static final String PREFS_USER = "prefs_name_user";
+  public static final String PREFS_USER_KEY_AVATAR_URL = "avatar_url";
+  public static final String PREFS_USER_KEY_AVATAR_WHOLE_URL = "avatar_whole_url";
+  public static final String PREFS_USER_KEY_ENCODED_PHONE_NUMBER = "encoded_phone_number";
+  public static final String PREFS_USER_KEY_NICKNAME = "nickname";
+  public static final String PREFS_USER_KEY_GENDER = "gender";
+  
+  
+}
